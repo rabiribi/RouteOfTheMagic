3819ae8cc533540c735004434e1d2df6d259cb23
setsuna <chenjiali2013@iCloud.com>
2018-05-05 20:51:27 +0800

    怪物buff更新，加入史莱姆双手剑士剧毒大蜘蛛，攻击部分未对接

---
+++ Assets/Scripes/Monster/Minister.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RouteOfTheMagic
+{
+    [RequireComponent(typeof(Monster))]
+    public class Minister : MonoBehaviour
+    {
+        Monster monster;
+        // Use this for initialization
+        void Start()
+        {
+            monster = GetComponent<Monster>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void attackTurn()
+        {
+            recoverySkill();
+            monster.attackPlayer(Monster.AttackType.Random);
+            return;         //补写攻击返回攻击点与攻击力
+        }
+
+        public void recoverySkill()
+        {
+            //获取所有怪物的monster，然后回血
+        }
+    }
+}
